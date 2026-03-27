9dcf4584fd08f910bb6dd7b9621b9171f9f1feab
adrian6922 <vargaszambranoj@gmaail.com>
2026-03-26 20:59:04 -0400

    repaso modulo 4

---
+++ Modulo_4/repaso.py
@@ -0,0 +1,12 @@
+persona = {
+    "nombre": "Juan",
+    "edad": 30,
+    "ciudad": "Madrid"
+}
+# Acceder a los valores del diccionario
+print(persona["nombre"])  # Imprime: Juan
+# print(persona["profesion"])  # Esto generará un error KeyError
+
+objetos = ["Libro", "Lápiz", "Cuaderno"]
+print(objetos[0])  # Imprime: Libro
+print(objetos[3])  # Esto generará un error IndexError
